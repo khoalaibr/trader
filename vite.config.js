4ebece354393e7142bbe1f5e38b351189886d454
khoalaibr <khoalai.br@gmail.com>
2025-01-01 22:29:48 -0300

    Subo docs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/trading-frontend/',
+  base: '/trader/',
   build: {
     outDir: 'docs', // Si estás usando 'docs' para GitHub Pages
   },
