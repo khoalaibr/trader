8c1f023b5754187291ae71220533cab77f9a487a
khoalaibr <khoalai.br@gmail.com>
2025-01-01 22:25:34 -0300

    Primera subida a github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/trading-frontend/',
+  build: {
+    outDir: 'docs', // Si estás usando 'docs' para GitHub Pages
+  },
 })
